e1949d099a2cd5a574fde3d7cf38d9be1d247466
welniasty99 <mwelna99@gmail.com>
2019-11-23 12:34:14 +0100

    Comminting Servers skeleton and unittests for project

--- Servers_python/servers_skeleton.py
+++ Servers_python/servers_skeleton.py
@@ -1,2 +1,93 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+from typing import Optional, List, Dict
+from abc import ABC, abstractmethod
+import re
+
+
+class Product:
+    def __init__(self, name: str, price: float):
+        self.name = name
+        self.price = price
+
+
+class TooManyProductsFoundError(Exception):
+    pass
+
+class NotEnoughProductsFoundError(Exception):
+    pass
+
+
+class Server(ABC):
+    def __init__(self, product_list: List[Product]):
+        self.product_list = product_list
+
+    def list_to_dict(self) -> Dict[str,Product]:
+        dict_prod = {}
+        for product in self.product_list:
+            dict_prod[product.name] = product
+        return dict_prod
+
+    n_max_returned_entries = 3
+
+    @abstractmethod
+    def get_searched_products(self, n: int = 1):
+        pass
+
+
+class ListServer(Server):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def get_searched_products(self, n :int) -> List[Product]:
+        list_products = []
+        for prod in self.product_list:
+            letters = re.split('(\d+)', prod.name)[0]
+            numbers = re.split('(\d+)', prod.name)[1]
+            if len(letters) == n and 2<=len(numbers)<=3:
+                list_products.append(prod)
+        if len(list_products) > self.n_max_returned_entries:
+            raise TooManyProductsFoundError
+        if not list_products:
+            raise NotEnoughProductsFoundError
+        return sorted(list_products, key = lambda product: product.price)
+
+
+class MapServer(Server):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.dict_prod = self.list_to_dict()
+
+    def get_searched_products(self, n: int) -> List[Product]:
+        list_products = []
+        for name in self.dict_prod.keys():
+            letters = re.split('(\d+)', name)[0]
+            numbers = re.split('(\d+)', name)[1]
+            if len(letters) == n and 2<=len(numbers)<=3:
+                list_products.append(self.dict_prod[name])
+        if len(list_products) > self.n_max_returned_entries:
+            raise TooManyProductsFoundError
+        if not list_products:
+            raise NotEnoughProductsFoundError
+        return sorted(list_products, key = lambda product: product.price)
+
+
+
+class Client:
+    def __init__(self, server : Server):
+        self.server = server
+
+    def get_total_price(self, n_letters: Optional[int]) -> Optional[float]:
+        try:
+            searched_products = self.server.get_searched_products(n_letters)
+        except TooManyProductsFoundError:
+            return None
+        except NotEnoughProductsFoundError:
+            return None
+
+        total_price = 0
+        for prod in searched_products:
+            total_price+=prod.price
+
+        return total_price
