7d44c0a091fc02005240b1eef8c8712862147c65
welniasty99 <mwelna99@gmail.com>
2019-12-03 14:17:52 +0100

    Adding final version of Servers with some fabulous tests

--- Servers_python/servers_tests.py
+++ Servers_python/servers_tests.py
@@ -3,7 +3,7 @@
 import unittest
 from collections import Counter
 
-from servers_skeleton import Server, ListServer, Product, Client, MapServer
+from servers_skeleton import Server, ListServer, Product, Client, MapServer, TooManyProductsFoundError, NotEnoughProductsFoundError
 
 server_types = (ListServer, MapServer)
 
@@ -19,12 +19,61 @@ class ServerTest(unittest.TestCase):
 
 
 class ClientTest(unittest.TestCase):
+
     def test_total_price_for_normal_execution(self):
         products = [Product('PP234', 2), Product('PP235', 3)]
         for server_type in server_types:
             server = server_type(products)
             client = Client(server)
             self.assertEqual(5, client.get_total_price(2))
+
+
+class TooManyProductsTest(unittest.TestCase):
+
+    def test_raise_error(self):
+        products = [Product('PP234',2), Product('PP235',3),Product('PP236',4),Product('PP237',5)]
+        for server_type in server_types:
+            server = server_type(products)
+            with self.assertRaises(TooManyProductsFoundError):
+                prods = server.get_searched_products(2)
+
+
+class CheckIfSorted(unittest.TestCase):
+
+    def test_IfSorted(self):
+        products = [Product('PP234',500), Product('PP235',520), Product('PP236',30)]
+        for server_type in server_types:
+            server = server_type(products)
+            prods = server.get_searched_products(2)
+            self.assertEqual([products[2],products[0],products[1]],prods)
+
+class CheckIfisNoneinListServer(unittest.TestCase):
+
+    def test_None(self):
+        products = [Product('PP234', 2), Product('PP235', 3), Product('PP236', 4), Product('PP237', 5)]
+        server = ListServer(products)
+        client = Client(server)
+        prod = client.get_total_price(2)
+        self.assertEqual(prod,None)
+
+class CheckIfisNoneinMapServer(unittest.TestCase):
+
+    def test_None(self):
+        products = [Product('PP234', 2), Product('PP235', 3), Product('PP236', 4), Product('PP237', 5)]
+        server = MapServer(products)
+        client = Client(server)
+        prod = client.get_total_price(2)
+        self.assertEqual(prod,None)
+
+class NotEnoughProductsTest(unittest.TestCase):
+
+    def test_raise_error(self):
+        products = []
+        for server_type in server_types:
+            server = server_type(products)
+            with self.assertRaises(NotEnoughProductsFoundError):
+                prods = server.get_searched_products(2)
+
 
 
 if __name__ == '__main__':
